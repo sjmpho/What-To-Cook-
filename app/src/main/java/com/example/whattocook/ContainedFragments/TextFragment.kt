356bfe722d97d64b94ae6369cef9491f4d9612cc
sjmpho <redbullmphosengata33@gmail.com>
2025-03-13 17:07:02 +0200

    added ingredients holder for search

--- app/src/main/java/com/example/whattocook/ContainedFragments/TextFragment.kt
+++ app/src/main/java/com/example/whattocook/ContainedFragments/TextFragment.kt
@@ -1,19 +1,24 @@
 package com.example.whattocook.ContainedFragments
 
+
+import android.R
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whattocook.APIServices.SpoonacularApiService
 import com.example.whattocook.Models.RecipesAdapter
 import com.example.whattocook.Models.Utility
-import com.example.whattocook.R
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,11 +26,15 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.core.view.isEmpty
+
 
 class TextFragment : Fragment() {
 
     lateinit var ingredient_input : EditText
+    lateinit var ingridents_layout :LinearLayout
     lateinit var Sublit:Button
+    lateinit var ingredients_list : ArrayList<String>
     private lateinit var recipesRecyclerView: RecyclerView
     private  lateinit var recipesAdapter: RecipesAdapter
     lateinit var ingredients_str :String
@@ -40,22 +49,28 @@ class TextFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        var view =  inflater.inflate(R.layout.fragment_text, container, false)
+        var view =  inflater.inflate(com.example.whattocook.R.layout.fragment_text, container, false)
 
-        ingredient_input = view.findViewById(R.id.ingredients_input)
-        Sublit = view.findViewById(R.id.submit_btn)
-        recipesRecyclerView = view.findViewById(R.id.LL_contain_suggestions)
-        floatingActionButton = view.findViewById(R.id.toScanPage)
+        ingredient_input = view.findViewById(com.example.whattocook.R.id.ingredients_input)
+        Sublit = view.findViewById(com.example.whattocook.R.id.submit_btn)
+        recipesRecyclerView = view.findViewById(com.example.whattocook.R.id.LL_contain_suggestions)
+        floatingActionButton = view.findViewById(com.example.whattocook.R.id.toScanPage)
         recipesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         recipesAdapter = RecipesAdapter(emptyList(),requireContext()) // Initialize with empty list
         recipesRecyclerView.adapter = recipesAdapter
+        ingridents_layout = view.findViewById(com.example.whattocook.R.id.ll_layout)
+
+        ingredients_list = arrayListOf()
+
 
         Sublit.setOnClickListener{
 
             ingredients_str = ingredient_input.text.toString()
             if(!ingredients_str.isEmpty()){
 
-                findRecipes(ingredients_str)
+                addWordLayout(ingredients_str)
+               // findRecipes(ingredients_str)
+
                 ingredient_input.setText("")
             }else{
 
@@ -65,7 +80,67 @@ class TextFragment : Fragment() {
 
         return view
     }
+    fun addWordLayout(word: String?) {
 
+        val currentRow = getCurrentRow(ingridents_layout)
+
+
+        val inflater = LayoutInflater.from(requireContext())
+        val wordView: View = inflater.inflate(com.example.whattocook.R.layout.ingredient_box, currentRow, false)
+
+
+        val wordText = wordView.findViewById<TextView>(com.example.whattocook.R.id.ingredient_name)
+        wordText.text = word
+        ingredients_list.add(word.toString())
+        Log.d("dagger", "addWordLayout: aray ${ingredients_list}")
+
+        val removeButton = wordView.findViewById<ImageButton>(com.example.whattocook.R.id.remove_ingredient_btn)
+        removeButton.setOnClickListener {
+            val wordText = wordView.findViewById<TextView>(com.example.whattocook.R.id.ingredient_name)
+            ingredients_list.remove(wordText.text)
+            Log.d("dagger", "addWordLayout: aray ${ingredients_list}")
+            currentRow.removeView(wordView)
+
+            if (currentRow.isEmpty()) {
+
+                ingridents_layout.removeView(currentRow)
+            }
+        }
+
+
+        currentRow.addView(wordView)
+    }
+
+
+    private fun getCurrentRow(parentLayout: LinearLayout): LinearLayout {
+        if (parentLayout.isEmpty()) {
+
+            return createNewRow(parentLayout)
+        }
+
+
+        val lastRow = parentLayout.getChildAt(parentLayout.childCount - 1) as LinearLayout
+
+
+        return if (lastRow.childCount < 4) {
+            lastRow
+        } else {
+
+            createNewRow(parentLayout)
+        }
+    }
+
+
+    private fun createNewRow(parentLayout: LinearLayout): LinearLayout {
+        val newRow = LinearLayout(requireContext())
+        newRow.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        newRow.orientation = LinearLayout.HORIZONTAL
+        parentLayout.addView(newRow)
+        return newRow
+    }
     private fun findRecipes(ingredients: String) {
 
         CoroutineScope(Dispatchers.IO).launch {
