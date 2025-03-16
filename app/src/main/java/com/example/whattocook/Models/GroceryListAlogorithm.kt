76568f1d23c1f86245b86c068fb3f7c77263a8cc
sjmpho <redbullmphosengata33@gmail.com>
2025-03-16 10:59:07 +0200

    Algorithm build

---
+++ app/src/main/java/com/example/whattocook/Models/GroceryListAlogorithm.kt
@@ -0,0 +1,15 @@
+package com.example.whattocook.Models
+
+class GroceryListAlogorithm {
+
+    companion object{
+
+
+        fun startAlgorithm(){
+            //retrivene favourites and book marked
+            //retrive each of the ingedients of each meal.
+            //
+
+        }
+    }
+}
