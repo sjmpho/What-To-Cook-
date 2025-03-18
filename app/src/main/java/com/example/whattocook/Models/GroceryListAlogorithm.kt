49a271d3758abc3d85b992a9ff4f244aab4e7c61
sjmpho <redbullmphosengata33@gmail.com>
2025-03-18 13:39:25 +0200

    get ingredients

--- app/src/main/java/com/example/whattocook/Models/GroceryListAlogorithm.kt
+++ app/src/main/java/com/example/whattocook/Models/GroceryListAlogorithm.kt
@@ -3,6 +3,9 @@ package com.example.whattocook.Models
 import android.app.DownloadManager.Query
 import android.util.Log
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.whattocook.APIServices.SpoonacularApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.Objects
 
 class GroceryListAlogorithm {
@@ -12,14 +15,23 @@ private lateinit var HashOfFavourites : HashMap<String,Objects>
         private lateinit var HashOfBookmakerd : HashMap<String,Objects>
 private lateinit var arrayOfFavourites : ArrayList<HashMap<String,Objects>>
         private lateinit var arrayOfBookMarked : ArrayList<HashMap<String,Objects>>
-
+        private lateinit var ingrdents : HashMap<String,Objects>;
+        private val spoonacularApiService: SpoonacularApiService by lazy {
+            Retrofit.Builder()
+                .baseUrl("https://api.spoonacular.com/") // Spoonacular base URL
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(SpoonacularApiService::class.java)
+        }
         fun startAlgorithm(){
 
             arrayOfFavourites = arrayListOf()
+            arrayOfBookMarked = arrayListOf()
             //retrivene favourites and book marked
             //retrive each of the ingedients of each meal.
             //
         fetchFavourites()
+            fetchBookMarked()
         }
 
         private fun fetchFavourites() {
@@ -38,23 +50,31 @@ private lateinit var arrayOfFavourites : ArrayList<HashMap<String,Objects>>
                 }
 
             }
-        }
-    private fun fetchBookMarked() {
-        // retrive favourites
-        Utility.getBookmarked().get().addOnSuccessListener {result ->
-            for (document in result) {
-                Log.d("Firestore", "${document.id} => ${document.data}")
-                HashOfFavourites.clear()
-                HashOfFavourites.put("id", document.get("id") as Objects)
-                HashOfFavourites.put("timestamp", document.get("timeStamp") as Objects)
-                arrayOfFavourites.add(HashOfFavourites)
-            }
-        }
-            .addOnFailureListener { exception ->
-                Log.w("Firestore", "Error getting documents: ", exception)
-            }
 
-    }
+        private fun fetchBookMarked() {
+            // retrive favourites
+            Utility.getBookmarked().get().addOnSuccessListener {result ->
+                for (document in result) {
+                    Log.d("Firestore", "${document.id} => ${document.data}")
+                    HashOfBookmakerd.clear()
+                    HashOfBookmakerd.put("id", document.get("id") as Objects)
+                    HashOfBookmakerd.put("timestamp", document.get("timeStamp") as Objects)
+                    arrayOfBookMarked.add(HashOfBookmakerd)
+                }
+            }
+                .addOnFailureListener { exception ->
+                    Log.w("Firestore", "Error getting documents: ", exception)
+                }
+
+        }
+        private fun getIngredients (mealID : Int){
+
+            val call1 = spoonacularApiService.getRecipeIngredients(mealID, Utility.ApiKey)
+
+
+        }
+        }
+
 }
 
 
